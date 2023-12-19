e9ebe8b495aa236f5c188495c357fe1b95ec0252
sudo-odner <odner.main@gmail.com>
2023-12-19 13:59:49 +0300

    add slices

--- array.go
+++ array.go
@@ -22,5 +22,23 @@ func main() {
 	newArray := array
 	
 	array[2] = "test"
-	fmt.Println("age", newArray)
+	fmt.Println("Copy arrays:", array, newArray)
+
+	// Declaring Slices
+	var slice []string
+	var slice_first = []string{"1", "2", "3"}
+	slice_second := []string{"1", "2", "3"}
+	fmt.Println(slice, slice_first, slice_second)
+
+	myslice := make([]string, 3) //a slice of 3 empty strings
+	new_slice_first := append(slice_first, "test", "testd")
+	fmt.Println(myslice, slice_first, new_slice_first)
+	slice_first[1] = "4"
+	fmt.Println(myslice, slice_first, new_slice_first)
+
+	// some test
+	array_test := [3]string{"First", "Second", "Third"}
+	mySlice := array_test[:]
+	array_test[1] = "4"
+	fmt.Println(mySlice, array_test)
 }
