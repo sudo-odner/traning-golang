5caf9ea019880f36c42ff40ba8dc59c7c4035961
sudo-odner <odner.main@gmail.com>
2023-12-21 15:12:51 +0300

    Изменил код кодирования, т.к В прошлй раз те так понял как он делается

--- codeVigenere/main.go
+++ codeVigenere/main.go
@@ -2,16 +2,82 @@ package main
 
 import (
 	"fmt"
+	"unicode"
+	"strings"
 	"unicode/utf8"
 )
 
-func codeInCodeVigenere(message string, key string) string{
-	var sizeMessage = utf8.RuneCountInString(message)
-	var newMessage = make([]rune, 0, sizeMessage)
+func genereteKeyByMessage(message string, key string) []rune {
+	var sliceMassage = []rune(message)
 	var sliceKey = []rune(key)
 
-	for i := 0; i < sizeMessage; i++ {
-		newMessage = append(newMessage, sliceKey[(i % len(sliceKey))])
+	var newKey = make([]rune, 0, len(sliceMassage))
+
+	for i := 0; i < len(sliceMassage); i++ {
+		newKey = append(newKey, sliceKey[(i % len(sliceKey))])
+	}
+
+	return newKey
+}
+
+func codingSymbolByCodeVigenere(symbolFirst rune, symboleSecond rune) rune{
+	if 'A' <= symbolFirst && symbolFirst <= 'Z' {
+		var differenceSymbol rune = (symbolFirst - unicode.ToUpper(symboleSecond))
+		if differenceSymbol < 0 {
+			differenceSymbol *= -1
+		}
+		var newSymbol = symbolFirst + differenceSymbol
+		if newSymbol > 'Z' {
+			newSymbol -= 26
+		}
+		return newSymbol
+	}
+
+	var differenceSymbol rune = symbolFirst - symboleSecond
+	if differenceSymbol < 0 {
+		differenceSymbol *= -1
+	}
+	var newSymbol = symbolFirst + differenceSymbol
+	if newSymbol > 'z' {
+		newSymbol -= 26
+	}
+	return newSymbol
+}
+
+func runeInSlice(desired rune, slice []rune) bool {
+	for i := 0; i < len(slice); i++ {
+		if slice[i] == desired {
+			return false
+		}
+	}
+	return true
+}
+
+func checkAnotherSymol(message string) bool {
+	var flag int = utf8.RuneCountInString(message)
+	for _, symbol := range message {
+		var unaccessSymbol = []rune("!@#$%^&*()[]{}|<>.,/±§œ∑´®†¥¨ˆøπ“‘åß∂ƒ©˙∆˚¬…æ«≈ç√∫˜µ≤≥÷")
+		if runeInSlice(symbol, unaccessSymbol){
+			flag -= 1
+		}
+	}
+	if flag == 0{
+		return true
+	}
+	return false
+}
+
+func codeInCodeVigenere(message string, key string) string{
+	if !checkAnotherSymol(message) {
+		return "Err in message, has unaccess symbol"
+	}
+	var sliceMassage = []rune(message)
+	var sliceKey = genereteKeyByMessage(message, strings.ToLower(key))
+
+	var newMessage = make([]rune, 0, len(sliceMassage))
+
+	for i := 0; i < len(sliceMassage); i++ {
+		newMessage = append(newMessage, codingSymbolByCodeVigenere(sliceMassage[i], sliceKey[i]))
 	}
 	fmt.Println(string(newMessage))
 	return ""
