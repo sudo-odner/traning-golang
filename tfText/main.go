3ff305ba6e94cee1c6886ad105b4d386c317cb44
sudo-odner <odner.main@gmail.com>
2023-12-22 18:11:22 +0300

    Добавил TF и красивый вывод

--- tfText/main.go
+++ tfText/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"strings"
 	"fmt"
-
 )
+
 func cleanUnaccessSymbol(text string, dataUnaccessSymbol []string) string {
 	var claenText string = text
 	for _, symbol := range dataUnaccessSymbol {
@@ -13,17 +13,31 @@ func cleanUnaccessSymbol(text string, dataUnaccessSymbol []string) string {
 
 	return claenText
 }
-func tf(text string) string { // map[string]int
+
+func printTF(dataMap map[string]int) {
+	fmt.Println("======================")
+	for key, count := range dataMap {
+		fmt.Printf("|%16v:%3v|\n", key, count)
+	}
+	fmt.Println("======================")
+}
+
+func tf(text string) map[string]int {
 	var lowerText = strings.ToLower(text)
 	var cleanText string = cleanUnaccessSymbol(lowerText, strings.Split("!@#$%^&*()))))_+-=±§><|[]?/,.;:~`", ""))
 	var dataWordText = strings.Split(cleanText, " ")
 
 	fmt.Println(dataWordText)
-	return ""
+	mapWordCoutnInText := make(map[string]int)
+
+	for _, word := range dataWordText {
+		mapWordCoutnInText[word]++
+	}
+	return mapWordCoutnInText
 }
 
 func main() {
 	var inputText string = "As far as eye could reach he saw nothing but the stems of the great plants about him receding in the violet shade, and far overhead the multiple transparency of huge leaves filtering the sunshine to the solemn splendour of twilight in which he walked. Whenever he felt able he ran again; the ground continued soft and springy, covered with the same resilient weed which was the first thing his hands had touched in Malacandra. Once or twice a small red creature scuttled across his path, but otherwise there seemed to be no life stirring in the wood; nothing to fear—except the fact of wandering unprovisioned and alone in a forest of unknown vegetation thousands or millions of miles beyond the reach or knowledge of man."
-	inputText = "Привет мир, я надеюсь не умереть"
-	tf(inputText)
+	//inputText = "Привет мир, я надеюсь не умереть"
+	printTF(tf(inputText))
 }
