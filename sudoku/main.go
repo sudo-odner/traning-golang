d449909f9ea0a30bbd6f4aa0ba45d6376bb04838
sudo-odner <odner.main@gmail.com>
2024-02-05 17:43:05 +0300

    add beautiful print

--- sudoku/main.go
+++ sudoku/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
 type cellFiledSudoku struct {
@@ -21,8 +22,8 @@ func (cell *cellFiledSudoku) setCell(value int8) error {
 	return nil
 }
 
-func (cell *cellFiledSudoku) getCell() (int8, bool, error) {
-	return cell.data, cell.accessChange, nil
+func (cell *cellFiledSudoku) getCell() int8 {
+	return cell.data
 }
 
 type areaSudoku struct {
@@ -40,18 +41,60 @@ func (area *areaSudoku) setCell(X, Y, value int8) {
 	}
 }
 
+func (area *areaSudoku) returnArea() {
+	var mainLine = strings.Repeat("-", 41)
+	var otherLine = "[]---|---|---[]---|---|---[]---|---|---[]"
+
+	for y, yData := range area.area {
+		if y%3 == 0 {
+			fmt.Println(mainLine)
+		} else {
+			fmt.Println(otherLine)
+		}
+
+		for x, xData := range yData {
+			value := xData.getCell()
+
+			if x%3 == 0 {
+				fmt.Printf("[] %v ", value)
+			} else {
+				fmt.Printf("| %v ", value)
+			}
+
+			/*	Without zero
+					if x%3 == 0 {
+						if value == 0 {
+							fmt.Print("[]   ")
+						} else {
+							fmt.Printf("[] %v ", value)
+						}
+					} else {
+						if value == 0 {
+							fmt.Print("|   ")
+						} else {
+							fmt.Printf("| %v ", value)
+						}
+					}
+				}
+			*/
+		}
+		fmt.Print("[]\n")
+	}
+	fmt.Println(mainLine)
+}
+
 func NewSudoku(area [9][9]int8) areaSudoku {
 	var newAreaSudoku [9][9]cellFiledSudoku
 
-	for Y, yArea := range area {
-		for X, xArea := range yArea {
+	for y, yArea := range area {
+		for x, xArea := range yArea {
 			if xArea != 0 {
-				newAreaSudoku[Y][X] = cellFiledSudoku{
+				newAreaSudoku[y][x] = cellFiledSudoku{
 					data:         xArea,
 					accessChange: false,
 				}
 			} else {
-				newAreaSudoku[Y][X] = cellFiledSudoku{
+				newAreaSudoku[y][x] = cellFiledSudoku{
 					data:         xArea,
 					accessChange: true,
 				}
@@ -74,5 +117,5 @@ func main() {
 		{0, 0, 0, 0, 8, 0, 0, 7, 9},
 	})
 	game1.setCell(0, 2, 5)
-	fmt.Println(game1)
+	game1.returnArea()
 }
